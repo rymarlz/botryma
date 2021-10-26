d05cbeddf6b9770af8be7c6e0e2226fac3c030f7
unknown <gonzalo.rima@gmail.com>
2021-10-26 17:09:16 -0300

    ultimo

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/trading'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/angular-app-heroku/' }),
-);
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/trading/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
